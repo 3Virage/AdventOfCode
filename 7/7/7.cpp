4798f719ed22a366523f431ab95e640d7a0e90e5
Karol Radwan <karolradwan@gmail.com>
2016-03-31 11:02:26 +0200

    7 works fine

--- 7/7/7.cpp
+++ 7/7/7.cpp
@@ -6,15 +6,121 @@
 using namespace std;
 
 class node {
+public:
 	string name;
-	int value;
+	unsigned short int value;
+	bool ready;
+	node() :name(""), value(0),ready(0) {}
 };
 
 class gate {
+public:
 	node in1;
 	node in2;
 	node out;
-	enum operation;
+	string operation;
+	bool isdone;
 
-	int execute();
+	void execute() {
+		if (operation == "NOT")
+			out.value = ~in1.value;
+		else if (operation == "LSHIFT")
+			out.value = in1.value << in2.value;
+		else if (operation == "RSHIFT")
+			out.value = in1.value >> in2.value;
+		else if (operation == "AND")
+			out.value = in1.value & in2.value;
+		else if (operation == "OR")
+			out.value = in1.value | in2.value;
+		else
+			out.value = in1.value;
+		isdone = 1;
+	}
+
+	gate() :in1(node()), in2(node()), out(node()), operation(""), isdone(0) {}
+	
+	void print() { cout << in1.name<<"("<<in1.value <<")"<< " " << operation << " " << in2.name<<"(" << in2.value << ")"  << out.name <<"(" << out.value << ")"<< " " << endl; }
 };
+
+int main() {
+	ifstream data;
+	data.open("7.txt");
+	gate  gates[340];
+	string cmd;
+	bool outflag = 0;
+	bool in1flag = 1;
+	int i = 0;
+	while (data) {
+		data >> cmd;
+		cout << cmd << endl;
+		if (cmd == "NOT" || cmd == "AND" || cmd == "OR" || cmd == "LSHIFT" || cmd == "RSHIFT")
+			gates[i].operation = cmd;
+		else if (cmd == "->") {
+			outflag = 1;
+			in1flag = 1;
+			i++;
+		}
+		else
+			if (outflag) {
+				gates[i - 1].out.name = cmd;
+				outflag = 0;
+			}
+			else if (in1flag) {
+				gates[i].in1.name = cmd;
+				in1flag = 0;
+			}
+			else
+				gates[i].in2.name = cmd;
+	}
+	for (int i = 0; i <= 338; i++)
+		gates[i].print();
+
+	for (int i = 0; i <= 338; i++) {
+		if (isdigit(gates[i].in1.name[0])) {
+			gates[i].in1.value = atoi(gates[i].in1.name.c_str());
+			gates[i].in1.ready = 1;
+		}
+		if (isdigit(gates[i].in2.name[0])) {
+			gates[i].in2.value = atoi(gates[i].in2.name.c_str());
+			gates[i].in2.ready = 1;
+		}
+		if (isdigit(gates[i].out.name[0])) {
+			gates[i].out.value = atoi(gates[i].out.name.c_str());
+		}
+		gates[i].print();
+	}
+	for (int j = 0; j <= 338; j++) {
+		for (int i = 0; i <= 338; i++) {
+		//	gates[i].print();
+			if (!gates[i].isdone)
+				if ((gates[i].in1.ready && gates[i].in2.ready) || (gates[i].in1.ready && gates[i].operation == "NOT") || (gates[i].in1.ready && gates[i].operation == "LSHIFT") || (gates[i].in1.ready && gates[i].operation == "RSHIFT") || (gates[i].in1.ready && gates[i].operation == ""))
+				{
+					gates[i].execute();
+					//	getchar();
+
+					for (int k = 0; k <= 338; k++) {
+						if (gates[k].in1.name == gates[i].out.name) {
+							gates[k].in1.value = gates[i].out.value;
+							gates[k].in1.ready = 1;
+						}
+						if (gates[k].in2.name == gates[i].out.name) {
+							gates[k].in2.value = gates[i].out.value;
+							gates[k].in2.ready = 1;
+						}
+					}
+				}
+		}
+	}
+	
+	for (int i = 0; i <= 338; i++)
+		if (gates[i].out.name == "a"&& gates[i].isdone)
+			gates[i].print();
+
+//second star---------------------------------------------------------
+// I've only changed fourth line in txt file
+
+	getchar();
+	return 0;
+}
+
+
